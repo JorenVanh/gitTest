264ec3a6638e3f55b58a6178242db2c0b05df8f6
Laurena1997 <laurena.nijs@realdolmen.com>
2018-09-24 13:08:30 +0200

    method added

---
+++ src/main/java/com/mycompany/gitmaven/BranchClass.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gitmaven;
+
+/**
+ *
+ * @author LNSBG31
+ */
+public class BranchClass 
+{
+    public String name; 
+    public String versions; 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersions() {
+        return versions;
+    }
+
+    public void setVersions(String versions) {
+        this.versions = versions;
+    }
+    
+    
+}
